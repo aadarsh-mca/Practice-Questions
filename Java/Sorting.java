3c9de481d792155159c61c64f6d62bf06599621b
aadarsh-mca <aadarsh.mca.lnctu.2022@gmail.com>
2024-02-17 22:03:51 +0530

    Quick Sort (Method 2) Completed

--- Java/Sorting.java
+++ Java/Sorting.java
@@ -547,7 +547,143 @@ public class Sorting {
 
 
 
+    /**
+     * Way 2 of Quick Sort,
+     * 
+     * Here we first place the pivot element on the correct index,
+     * and then perform swapping of 
+     * smaller elements to left of pivot,
+     * and larger elements to the right of pivot,
+     * 
+     * and then recursively performing the above steps,
+     * until we sort the complete array
+     */
+    static void quickSortHelper2(int[] arr, int low, int high) {
+        if(low >= high) {
+            return;
+        }
 
-    
+        /**
+         * First select any element as pivot
+         */
+        int previousPivotIndex = low;
+
+        /**
+         * Now count the number of element, smaller than (or equal) pivot
+         * 
+         * Please Note :
+         * we have to count equal elements also,
+         * so that we also move all equal elements to left,
+         * to maintain consistency of smaller on left and larger on right
+         * 
+         * either move all equal elements on left, 
+         * or move all equal elements on right.
+         * * (Data must be consistent)
+         */
+        int leftElementCount = 0;
+        for(int i = low + 1; i <= high; i++) {
+            if(arr[previousPivotIndex] >= arr[i]) {
+                leftElementCount++;
+            }
+        }
+        
+        /**
+         * Here we counted the number of elements,
+         * that need to be on the left of pivot,
+         * 
+         * and then adding low + leftElementCount,
+         * so that the pivotIndex is not always fixed to any index,
+         * when we go deeper in sub-arrays
+         * 
+         * to tackle this,
+         * we are adding low so that we get index ranging from low - high
+         * and pivot lie in that particular sub-array only
+         * 
+         * that mean 
+         * now we have the correct index of pivot
+         * on which the pivot element should be placed
+         */
+        // System.out.println("Number of element that need to be on left of pivot are : " + leftElementCount);
+        int currentPivotIndex = low + leftElementCount;
+
+        /**
+         * Swapping the current pivot to its correct pivot index
+         */
+        swap(arr, previousPivotIndex, currentPivotIndex);
+
+        /**
+         * As we have placed the pivot at corrent index,
+         * now we can proceed with further steps.
+         * 
+         * We will arrange all the smaller (or equal) elements on the left of pivot,
+         * and all the larger elements on the right of pivot.
+         */
+        quickSortArrange(arr, currentPivotIndex, low, high);
+
+        quickSortHelper2(arr, low, currentPivotIndex - 1);
+        quickSortHelper2(arr, currentPivotIndex + 1, high);
+    }
+
+    static void quickSortArrange(int[] arr, int pivotIndex, int left, int right) {
+        /**
+         * Here we will arrange the elements 
+         * until left and right both reaches pivot index,
+         */
+        while(left <= pivotIndex && right >= pivotIndex) {
+
+            /**
+             * As we already assumed that, 
+             * all the (equal) elements will also be on left of pivot element,
+             * so simply skip the equal elements which are on left.
+             */
+            if(arr[pivotIndex] >= arr[left]) {
+                left++;
+
+            /**
+             * But we cannot skip equal elements on right,
+             * because we have assumed to have smaller (or equal) elements on left.
+             * Therefore we will only skip greater ('<') elements on right.
+             */
+            } else if(arr[pivotIndex] < arr[right]) {
+                right--;
+
+            /**
+             * Therefore,
+             * if we found smaller (or equal '>=') elements on right,
+             * we will move them on left side of pivot index.
+             */
+            } else if(arr[pivotIndex] < arr[left] && arr[pivotIndex] >= arr[right]) {
+                quickSwap(arr, left, right);
+                left++;
+                right--;
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * Heap Sort
+     * 
+     */
+    static int[] heapSort(int[] arr, int n) {
+
+
+    return arr;
+    }
+
+
+
+
 
 }
