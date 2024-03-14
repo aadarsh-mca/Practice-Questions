15841c8caa0d15101e945585368d5385aa3a470a
aadarsh-mca <aadarsh.mca.lnctu.2022@gmail.com>
2024-03-14 18:57:11 +0530

    Merge Sorted Array completed

--- Java/MergeSortedArray.java
+++ Java/MergeSortedArray.java
@@ -1,67 +1,113 @@
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Queue;
 
 public class MergeSortedArray {
     
     public static void main(String[] args) {
+        System.out.println("Merge both array : ");
+        /**
+         * Merge inside array 1
+         */
         // int nums1[] = {-1,0,0,0,3,0,0,0,0,0,0};  //  1,2,3,0,0,0   -1,0,0,0,3,0,0,0,0,0,0
         // int nums1[] = {-1,0,3,0,0,0};  //  1,2,3,0,0,0   -1,0,0,0,3,0,0,0,0,0,0
-        int nums1[] = {1,2,3,0,0,0};  //  1,2,3,0,0,0   -1,0,0,0,3,0,0,0,0,0,0
+        // int nums1[] = {1,2,3,0,0,0};  //  1,2,3,0,0,0   -1,0,0,0,3,0,0,0,0,0,0
+        // int nums1[] = {1};  //  1,2,3,0,0,0   -1,0,0,0,3,0,0,0,0,0,0
+        int nums1[] = {0};  //  1,2,3,0,0,0   -1,0,0,0,3,0,0,0,0,0,0
         int m = nums1.length; 
         // int nums2[] = {-1,-1,0,0,1,2};  //  2,5,6   -1,-1,0,0,1,2
-        int nums2[] = {2,5,6};  //  2,5,6   -1,-1,0,0,1,2
+        // int nums2[] = {2,5,6};  //  2,5,6   -1,-1,0,0,1,2
+        // int nums2[] = {};  //  2,5,6   -1,-1,0,0,1,2
+        int nums2[] = {1};  //  2,5,6   -1,-1,0,0,1,2
         int n = nums2.length;
-        m = m - n;
+        
+        m = m-n;
+        mergeInArr1(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
+        
+        
+        System.out.println();
+        System.out.println("Sort both array in sequential way : ");
+        /**
+         * Don't merge, 
+         * just make array 1, 2 in such a way that, 
+         * if we print array 1, 2 in sequence it look like sorted array.
+         */
+        // long[] a = {1,8,8};
+        // long[] b = {2,3,4,5};
+        // long[] a = {0,6,6,6,6,6,7,8};
+        // long[] b = {5,5,6,6,8};
+        long[] a = {1,1,3,6,9,9};
+        long[] b = {1,2,3,4,8,8};
+        
+        shuffle_sort_12_withExtraSpace(a, b);
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
 
-        merge(nums1, m, nums2, n);
 
+        // Or else use Shell Sort
     }
 
-    static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m + n - 1;
-        int j = m - 1;
-        int k = n - 1;
-        while(k >= 0) { // untill nums2 get merged
-            if(nums1[j] <= nums2[k]) {
-                nums1[i] = nums2[k];
-                i--;
-                k--;
+    static void mergeInArr1(int[] nums1, int m, int[] nums2, int n) {
+        int last = m + n - 1;
+        m -= 1;
+        n -= 1;
+
+        while(n >= 0) {
+            if(m >= 0 && nums1[m] > nums2[n]) {
+                nums1[last] = nums1[m];
+                m--;
             } else {
-                nums1[i] = nums1[j];
-                i--;
-                j--;
+                nums1[last] = nums2[n];
+                n--;
+            }
+            last--;
+        }
+    }
+
+
+    /**
+     * Optimal approach,
+     * Just move all the smaller element 
+     * from 2nd array to the 1st array,
+     * then sort both array.
+     * 
+     * <p>We can shuffle the element by 
+     * comparing last element of 1st array
+     * and first element of 2nd array.
+     * 
+     * <p>Time Complexity : {@code O(min(n, m) + (n log n) + (m log m))}, wher n, m is the size of Array 1, 2 respectively
+     * <p>Space Complexity : {@code O(1)}
+     * 
+     * @param a
+     * @param b
+     */
+    static void shuffle_sort_12_withExtraSpace(long[] a, long[] b) {
+        int aLen = a.length;
+        int bLen = b.length;
+
+        int lastA = aLen-1;
+        int firstB = 0;
+
+        while(lastA >= 0 && firstB < bLen) {
+            if(a[lastA] > b[firstB]) {
+                swap(a, b, lastA, firstB);
+                lastA--;
+                firstB++;
+            } else {
+                break;
             }
         }
 
-        System.out.println("After : " + Arrays.toString(nums1));
+        Arrays.sort(a);
+        Arrays.sort(b);
     }
-
-    static void merge(int[] nums1, int m, int[] nums2, int n) {
-        System.out.println("Before : " + Arrays.toString(nums1));
-
-        System.out.println("m : " + m + ", n : " + n);
-
-        int i=0;
-        int j=0;
-        while(j < n) {
-            // if(nums1[i] != 0 && nums1[i] <= nums2[j]) {
-            //     i++;
-            // } else 
-            if(nums1[i] > nums2[j] || (i >= ((m+n) - (n-j)) && nums1[i] == 0)) {
-                int temp1 = nums1[i];
-                nums1[i] = nums2[j];
-                for(int k=i+1; k < m+n; k++) {
-                    int temp2 = nums1[k];
-                    nums1[k] = temp1;
-                    temp1 = temp2;
-                }
-                j++;
-            }
-            i++;
-        }
-
-        System.out.println("After : " + Arrays.toString(nums1));
+    
+    static void swap(long[] fromArr, long[] toArr, int fromIdx, int toIdx) {
+        long temp = fromArr[fromIdx];
+        fromArr[fromIdx] = toArr[toIdx];
+        toArr[toIdx] = temp;
     }
-
 
 
 }
